0c92299c3938bf3f54efdd4b9ee6560980c6e140
Dmytro Tsetsyk <merlinultra@gmail.com>
2018-11-18 14:06:40 +0200

    Updated server side for dupper ext.

---
+++ FFilms/Repositories/BaseRepo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace FFilms.Repositories
+{
+    public class BaseRepo
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public Task<List<T>> GetDataByQueryAsync<T>(string query)
+        {
+            return Task.Run(() => GetDataByQuery<T>(query));
+        }
+
+        public List<T> GetDataByQuery<T>(string query)
+        {
+            List<T> booksGenres = new List<T>();
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                booksGenres = db.Query<T>(query).ToList();
+            }
+            return booksGenres;
+        }
+    }
+}
--- FFilms/Repositories/GenresFilmsRepo.cs
+++ FFilms/Repositories/GenresFilmsRepo.cs
@@ -4,29 +4,18 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
-using Dapper;
 using FFilms.Repositories.Abstractions;
 using FFilms.Repositories.Models;
 
 namespace FFilms.Repositories
 {
-    public class GenresFilmsRepo : IGenresFilmsRepo
+    public class GenresFilmsRepo : BaseRepo, IGenresFilmsRepo
     {
         string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
-        public Task<List<GenresFilms>> GetGenresFilmsAsync()
+        public async Task<List<GenresFilms>> GetGenresFilmsAsync()
         {
-            return Task.Run(() => GetGenresFilms());
-        }
-
-        public List<GenresFilms> GetGenresFilms()
-        {
-            List<GenresFilms> booksGenres = new List<GenresFilms>();
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                booksGenres = db.Query<GenresFilms>("SELECT * FROM GenresFilmsListTable WHERE IsActive = 1").ToList();
-            }
-            return booksGenres;
+            return await GetDataByQueryAsync<GenresFilms>("SELECT * FROM GenresFilmsListTable WHERE IsActive = 1");
         }
     }
 }
